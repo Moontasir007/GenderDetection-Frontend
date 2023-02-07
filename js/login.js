53e365d9d02635d2dcc6da275192794aab57596f
Moontasir Mamun <moontasir.csecu@gmail.com>
2023-02-07 22:48:00 +0600

    ui complete

---
+++ js/login.js
@@ -0,0 +1,9 @@
+function mySubmit() {
+    const UserName = document.getElementById("userName").value;
+    const Password = document.getElementById("password").value;
+
+    if (UserName == 'Mamun' && Password == '12345') {
+        window.location.href = 'Home.html';
+    }
+
+}
